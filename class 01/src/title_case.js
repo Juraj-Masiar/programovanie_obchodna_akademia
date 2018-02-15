0d4a14db8f45065c7e796b1585a4049b0eeae1bc
Juraj Mäsiar <juraj.masiar@gmail.com>
2018-02-15 15:05:03 +0100

    - first class code is done

--- class 01/src/title_case.js
+++ class 01/src/title_case.js
@@ -2,4 +2,29 @@
 
 // More info about letter case: https://en.wikipedia.org/wiki/Letter_case
 
-console.log('hello world');
+// console.log('hello world');
+
+// console.log('hello world', 42);
+
+const testData = 'Kuzminová získala ďalšie striebro, Fialková skončila piata';
+console.log(testData);
+
+console.log('this is the result:\n', transformToTitleCase(testData));
+
+
+function transformToTitleCase(inputText) {
+  const result = inputText
+    .split(' ')
+    .map(function (word) {
+      const firstLetter = word.charAt(0);
+      const firstUpperCaseLetter = firstLetter.toLocaleUpperCase();
+      const restOfTheWord = word.slice(1);
+      return firstUpperCaseLetter + restOfTheWord;
+    })
+    .join(' ');
+  return result;
+}
+
+
+
+
