2bd9a435aadce03f139dd1e3923cfb0614572eb4
Juraj Mäsiar <juraj.masiar@gmail.com>
2018-03-08 13:32:34 +0100

    - added some logical operations

---
+++ class 03/src/logical_operators_and_values.js
@@ -0,0 +1,36 @@
+const noop = () => {};
+
+// Falsy values:
+if (false)      noop(); else console.log(`false is false`);
+if ('')         noop(); else console.log(`empty string is false`);
+if ("")         noop(); else console.log(`empty string is false`);
+if (``)         noop(); else console.log(`empty string is false`);
+if (0)          noop(); else console.log(`zero is false`);
+if (null)       noop(); else console.log(`null is false`);
+if (undefined)  noop(); else console.log(`undefined is false`);
+if (NaN)        noop(); else console.log(`Not a Number is false`);
+
+console.log('\n');
+// Truthy values are everything else, these are just few examples:
+if (true)                console.log('true is true');
+if (42)                  console.log('every number expect 0 is true');
+if ('not empty string')  console.log('not empty string is true');
+if (' ')                 console.log('even single space is not empty string');
+if ([])                  console.log('empty array is true');
+if ({})                  console.log('empty object is true');
+
+
+console.log('\n');
+// Logical operations:
+//    ||     - OR
+//    &&     - AND
+//    !      - NOT
+//    >      - bigger than
+//    >=     - bigger or equal
+//    <      - smaller than
+//    <=     - smaller or equal
+//    ===    - equal than
+//    !==    - not equal
+
+if (false || 1 === 1 && !false && 1 < 2 && 2 >= 0 && 1 !== 2)
+  console.log('this long expression if TRUE');
