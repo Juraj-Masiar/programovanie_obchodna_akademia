ed28493a9b9efd46288789d32d82c24499fd969e
Juraj Mäsiar <juraj.masiar@gmail.com>
2018-03-15 15:25:50 +0100

    - added helping coments to the homework file

--- class 01/src/homework/swapping_letters.js
+++ class 01/src/homework/swapping_letters.js
@@ -4,7 +4,7 @@
  *    There is only one rule: First and Last letter in every word needs to be correctly placed - all other letters can be shuffled.
  *
  *    Example input:    "Reading swapped words"
- *    Example output:   "Raeding sawpped wrods"
+ *    Example output:   "Raeding sawpepd wrods"
  *
  *    How it works? Read here:
  *    https://www.mrc-cbu.cam.ac.uk/people/matt.davis/cmabridge/
@@ -14,7 +14,22 @@
 
 function shuffleWords(inputText) {
   // your code goes here:
-  return '';
+
+    const result = inputText
+        .split(' ')
+        .map(function (word) {      // this is the transformation function
+            // extract first and last letter
+            // use slice funtion to get middle letters
+            // call call shuffle function
+            // return first + shuffle result + last
+        })
+        .join(' ');
+    return result;
+}
+
+function shuffle(input) {
+    // todo: finish proper shuffle
+    return input
 }
 
 const testInput = 'Reading swapped words';
