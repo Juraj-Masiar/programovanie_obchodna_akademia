5c75b88220867696eeefc41abcf9f91559c68fe2
Juraj Mäsiar <juraj.masiar@gmail.com>
2018-05-10 11:24:51 +0200

    - adding Snake and BodyPart API

--- class 11/snake/snake.js
+++ class 11/snake/snake.js
@@ -1,9 +1,44 @@
 
 const Snake = () => {
+  const _snakeBody = [BodyPart(true), BodyPart()];
+  let self;
+  return (self = {                            // Snake API
+    getSnake: () => _snakeBody.slice(),
+    getHead: () => _snakeBody.find(x => x.isHead()),
+    getTail: () => _snakeBody.filter(x => x.isBody()),
+    addBodyPart: addBodyPart,
+    removeBodyPart: removeBodyPart
+  });
+  function BodyPart(isHead = false) {
+    let _posX = 0, _posY = 0;
+    const _id = NumberGenerator();
+    const _node = buildElement('div', {id: _id, class: `snake_body_part ${isHead ? 'snake_head' : ''}`});   // body part is by default invisible (out of visible area), by setting correct position it will become visible
+    return {                                  // BodyPart API
+      isHead: () => isHead,
+      isBody: () => !isHead,
+      getPosition: () => [_posX, _posY],
+      setPosition: setPosition,
+      setPositionX: x => setPosition(x, _posY),
+      setPositionY: y => setPosition(_posX, y)
+    };
 
-  return {
-    getHead: noop
-  };
+    function setPosition(x, y) {
+      _posX = x;
+      _posY = y;
+      _node.style.left = px(x);   // todo: maybe extract graphics part to the separate component?
+      _node.style.top = px(y);
+    }
+  } // BodyPart
 
+  function addBodyPart(position) {
+    if (position < 0 || position + 1 >= _snakeBody.length) throw `Wrong insert position, cannot insert at ${position} if snake length is ${_snakeBody.length}`;
+    const bodyPart = BodyPart();
+    _snakeBody.splice(position + 1, 0, bodyPart);
+    return bodyPart;
+  }
+  function removeBodyPart(position) {
+    if (position < 0 || position + 1 >= _snakeBody.length) throw `Wrong remove position, cannot remove at ${position} if snake length is ${_snakeBody.length}`;
+    _snakeBody.splice(position + 1, 1);
+  }
 
-};
+};  // Snake
--- class 11/snake/utils.js
+++ class 11/snake/utils.js
@@ -5,4 +5,52 @@ const px = pixels => `${pixels}px`;
 const percent = percent => `${percent}%`;
 const timeoutPromise = delay => new Promise(resolve => setTimeout(resolve, delay));
 
+const NumberGenerator = (() => {
+  let i = 0;
+  return () => ++i;
+})();
+
+
+function removeChildNodes(node) {
+  while (node.hasChildNodes())
+    node.removeChild(node.lastChild);
+}
+function replaceChildNodes(node, nodesList) {
+  removeChildNodes(node);
+  if (!nodesList) return;                                   // if there is nothing to append, we just clear and exit
+  if (!Array.isArray(nodesList)) nodesList = [nodesList];   // upgrade to array
+  const fragment = document.createDocumentFragment();
+  nodesList.forEach(node => fragment.appendChild(node));
+  node.appendChild(fragment);
+}
+function createFragment(nodesArray) {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < nodesArray.length; i++) fragment.appendChild(nodesArray[i]);
+  return fragment;
+}
+
+/**
+ * NOTE:
+ * - event handlers needs to be wrapped under "handlers" key
+ * - dataset items like "data-custom-attr" are supported
+ */
+function buildElement(name, attrs, childNodesOpt) {
+  const node = name === 'svg' || name === 'path' ? document.createElementNS('http://www.w3.org/2000/svg', name) : document.createElement(name);   // SVG requires createElementNS because the default one would lowercase element names
+  if (attrs) {
+    if (typeof attrs === 'string') attrs = {textContent: attrs};    // allow using text content as second parameter
+    Object.entries(attrs).forEach(([key, value]) => {
+      switch (key) {
+        case 'text':  // falls through
+        case 'textContent': node[key] = value; break;                   // todo: find out why do we use this instead of "setAttribute"
+        case 'handlers': Object.assign(node, attrs.handlers); break;    // event handlers can be set only using `Object.assign` function!
+        default: node.setAttribute(key, value); break;                  // everything else using `setAttribute` function - NOTE: this can handle "data-stuff" AND read-only "width" in SVG nodes!
+      }
+    });
+  }
+  if (childNodesOpt) {
+    if (!Array.isArray(childNodesOpt)) childNodesOpt = [childNodesOpt];
+    childNodesOpt.forEach(childNode => {if (childNode) node.appendChild(childNode)});
+  }
+  return node;
+}
 
