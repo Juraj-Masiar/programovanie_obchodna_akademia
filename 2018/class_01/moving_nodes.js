8bdcf50d2f629abdfde1cae9f7e42cb7bc090b33
Juraj <juraj.masiar@gmail.com>
2018-11-19 16:18:31 +0100

    - added code for moving nodes

--- 2018/class_01/moving_nodes.js
+++ 2018/class_01/moving_nodes.js
@@ -1,3 +1,16 @@
 
 
 
+
+async function moveNodeFromUpDown(node) {
+  const timeoutPromise = waitingTime => new Promise(resolve => setTimeout(resolve, waitingTime));
+  node.style.position = 'fixed';
+
+  for (let i = 0; i < 1000; i++) {
+    node.style.top = `${i}px`;
+    // node.style.left = `${i}px`;
+    await timeoutPromise(50);
+  }
+}
+
+moveNodeFromUpDown({style: {}});
