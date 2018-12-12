4b3cc398bcd40de779eb67e311558cbf9536eb8f
Juraj Mäsiar <juraj.masiar@gmail.com>
2018-12-12 15:59:01 +0100

    - stuff from class

---
+++ 2018/class_01/playing with javascript.js
@@ -0,0 +1,9 @@
+
+const result = Array(10 + 40 + 1)
+    .fill()
+    .map((x, i) => 50 + i);
+
+console.log(result);
+
+
+
--- Games/typo/src/typo.js
+++ Games/typo/src/typo.js
@@ -2,10 +2,14 @@
 
 async function RUN_TYPO() {
   console.log('hello from typo.js script');
+  document.body.style.background = 'red';
 
-  const words = PageTextExtractor.getWords();
-  const wordsNodes = words.map(word => buildElement('div', {style: 'background: gray; color: white;'}))
-  
+
+  // todo: work in progress...
+  // const words = PageTextExtractor.getWords();
+  // const wordsNodes = words.map(word => buildElement('div', {style: 'background: gray; color: white;'}))
+
+
 }
 
 
