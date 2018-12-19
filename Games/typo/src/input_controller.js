db24c00166c996d7e41db472075b549352881071
Juraj Mäsiar <juraj.masiar@gmail.com>
2018-12-19 16:21:39 +0100

    - added code from the class

--- Games/typo/src/input_controller.js
+++ Games/typo/src/input_controller.js
@@ -5,6 +5,6 @@ const InputController = (() => {
 
   };
 
-  
+
 })();
 
--- Games/typo/src/typo.js
+++ Games/typo/src/typo.js
@@ -8,17 +8,29 @@ async function RUN_TYPO() {
   // todo: work in progress...
   const words = PageTextExtractor.getWords();
   const wordsNodes = words
-    .map(word => buildElement('span', {style: 'background: gray; color: white; display: inline-block; position: absolute;', textContent: word}));
+    .map(word => buildElement('span', {style: 'background: gray; color: white; display: inline-block; position: absolute;', textContent: word}))
+    .slice(0, 10);
 
   console.log('wn', wordsNodes, words);
 
-  const wordNode = wordsNodes[0];
-  document.body.prepend(wordNode);
+  // Animation of a single node:
+  // const wordNode = wordsNodes[0];
+  // document.body.prepend(wordNode);
+  // const animation = NodeAnimator.fromUpToDown(wordNode, px(window.innerWidth / 2), px(0), px(window.innerHeight), 10000);
+  // animation.finished.then(() => console.log('word animation done', wordNode));
 
-  const animation = NodeAnimator.fromUpToDown(wordNode, px(window.innerWidth / 2), px(0), px(window.innerHeight), 10000);
-
-  animation.finished.then(() => console.log('word animation done', wordNode));
-
+  // Animation of all nodes:
+  for (let i = 0; i < wordsNodes.length; i++) {
+    // extract node
+    const wordNode = wordsNodes[i];
+    // add it to the page body
+    document.body.prepend(wordNode);
+    // start the animation
+    const animation = NodeAnimator.fromUpToDown(wordNode, px(window.innerWidth / (i + 2)), px(i * 50), px(window.innerHeight + i * 50), 10000);
+    // todo: when animation is done, we can do something...
+    animation.finished.then(() => console.log('word animation done', wordNode));
+    
+  }
 
 
 }
