495c5029772923a2c00efb2d11f62e113419cbc0
Juraj Mäsiar <juraj.masiar@gmail.com>
2019-04-24 16:15:59 +0200

    - containers generator stuff, starts

--- 2018/class_04/containers.js
+++ 2018/class_04/containers.js
@@ -8,11 +8,29 @@ Set:      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Glob
 
  */
 
+let randomCallCount = 0;
+function getRandomBetween(min, max) {
+  randomCallCount++;
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+// 0 - 10 -> 3, 4, 1, 2, 5
 
+function generateNumbers(from, to, howMany) {
+  const result = [];
+  // WRONG CODE START:    ----------------------------------- //
+  while (result.length !== howMany) {
+    const x = getRandomBetween(from, to);
+    if (!result.includes(x))
+      result.push(x);
+  }
+  // WRONG CODE END       ----------------------------------- //
+  return result;
+}
+const {performance} = require('perf_hooks');
+const x = performance.now();
+console.log(generateNumbers(0, 10000, 10000));
+const diff = performance.now() - x;
+console.warn(diff / 1000);
 
-
-
-
-
-
+console.warn(`called ${randomCallCount} times!!!`);
 
--- 2018/class_04/stars.js
+++ 2018/class_04/stars.js
@@ -20,4 +20,20 @@ EXPECTED RESULT:
 // END
 */
 
+let result = '';
 
+function printStars(count, predicateFn) {
+  for (let i = 0; i < count; i++) {
+    for (let j = 0; j < count; j++) {
+      result += predicateFn(i, j) ? '*' : ' ';
+    }
+    result += '\n';
+  }
+}
+printStars(4, (i, j) => i === j);
+
+result += '\n';
+
+
+
+console.log(result);
