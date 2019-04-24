df9e671c9313dba10e6ea0b6d5499a8f9c4189cc
Juraj Mäsiar <juraj.masiar@gmail.com>
2019-04-24 13:52:49 +0200

    - containers

---
+++ 2018/class_04/containers.js
@@ -0,0 +1,18 @@
+
+/*
+MDN Documentation:
+Array:    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+Object:   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+Map:      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+Set:      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+
+ */
+
+
+
+
+
+
+
+
+
